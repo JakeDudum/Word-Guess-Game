945d47bd1b98b112c96fbfc28df8766984a1b506
Jake Dudum <jakedudum@gmail.com>
2019-05-22 16:37:03 -0700

    Added var computerWord which picks a random word from words array and added eventlistener for document.onkeyup which stores the key pressed for now in game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,7 @@
 var words = ["naruto", "bleach", "luffy", "kurosaki", "gon", "alchemist", "hunter", "bankai", "kamehameha", "dragon", "saiyan", "vegeta", "goku", "rasengan", "mugetsu", "bazooka", "navy", "marines", "roger"];
 
+var computerWord = words[Math.floor(Math.random() * words.length)];
+
+document.onkeyup = function (event) {
+    var key = event.key;
+}
