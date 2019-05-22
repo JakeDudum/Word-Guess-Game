6b87d1a34080206561f0f4b351b95ef06339ae53
Jake Dudum <jakedudum@gmail.com>
2019-05-22 16:33:12 -0700

    Created a variable array of words and filled it with anime themed words

---
+++ assets/javascript/game.js
@@ -0,0 +1,2 @@
+var words = ["naruto", "bleach", "luffy", "kurosaki", "gon", "alchemist", "hunter", "bankai", "kamehameha", "dragon", "saiyan", "vegeta", "goku", "rasengan", "mugetsu", "bazooka", "navy", "marines", "roger"];
+
