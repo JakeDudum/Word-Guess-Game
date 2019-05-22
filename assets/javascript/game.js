c1f8ab1172cadbf097598d817b669d462ce760a0
Jake Dudum <jakedudum@gmail.com>
2019-05-22 16:39:05 -0700

    Moved computerWord into eventlistener in game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,9 @@
 var words = ["naruto", "bleach", "luffy", "kurosaki", "gon", "alchemist", "hunter", "bankai", "kamehameha", "dragon", "saiyan", "vegeta", "goku", "rasengan", "mugetsu", "bazooka", "navy", "marines", "roger"];
 
-var computerWord = words[Math.floor(Math.random() * words.length)];
-
 document.onkeyup = function (event) {
     var key = event.key;
+
+    var computerWord = words[Math.floor(Math.random() * words.length)];
+
+    
 }
